121ee7dd52aebdeb0289015860110b9ea16373b3
kytpbs <kytpbs@gmail.com>
2023-01-23 00:14:10 +0300

    Deleted unnecessary files.

--- Read.py
+++
@@ -1,5 +0,0 @@
-def readFile(fileName):
-    fileObj = open(fileName, "r")  # opens the file in read mode
-    words = fileObj.read().splitlines()  # puts the file into an array
-    fileObj.close()
-    return words
