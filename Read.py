bee17e3cd4cdbde308a3743b5082dd17da2be62a
kytpbs <kytpbs@gmail.com>
2023-01-23 16:00:03 +0300

    Create Read.py

---
+++ Read.py
@@ -0,0 +1,5 @@
+def readFile(fileName):
+    fileObj = open(fileName, "r")  # opens the file in read mode
+    words = fileObj.read().splitlines()  # puts the file into an array
+    fileObj.close()
+    return words
