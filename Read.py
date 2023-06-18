824a7c1c49c30c45a4e519a84ea941e29ac7c0bd
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-06-18 15:17:42 +0300

    HOTFIX
    FUCKED UP BIRTHDAYS

--- Read.py
+++ Read.py
@@ -16,22 +16,3 @@ def jsonRead(name: str):
 def log(data: str):
   with open("log.txt", "a") as f:
     f.write(data + "\n")
-
-def get_user_and_date_from_string(dict: dict):
-  new_dict = {}
-  for user_id, date in dict.items():
-    user = Client.get_user(int(user_id))
-    if user is None:
-      continue
-    dates = date.split("-")
-    if len(dates) != 3:
-      e = ValueError("Hatalı tarih formatı, lütfen düzeltin!")
-      print(e)
-      continue
-    date_obj = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
-    print(f"{user} : {date_obj}")
-    if date_obj is None:
-      continue
-    new_dict[user] = date_obj
-
-  return new_dict
--- main.py
+++ main.py
@@ -377,6 +377,26 @@ class MyClient(discord.Client):
 client = MyClient()
 tree = app_commands.CommandTree(client)
 
+def get_user_and_date_from_string(dict: dict):
+  new_dict = {}
+  for user_id, date in dict.items():
+    user = client.get_user(int(user_id))
+    if user is None:
+      continue
+    dates = date.split("-")
+    if len(dates) != 3:
+      e = ValueError("Hatalı tarih formatı, lütfen düzeltin!")
+      print(e)
+      continue
+    date_obj = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
+    print(f"{user} : {date_obj}")
+    if date_obj is None:
+      continue
+    new_dict[user] = date_obj
+
+  return new_dict
+
+
 @tasks.loop(time= time(hour=6,minute=30, tzinfo=timezone.utc)) #9.30 for +3 timezone
 async def check_birthdays():
     genel = client.get_channel(general_chat_id)
