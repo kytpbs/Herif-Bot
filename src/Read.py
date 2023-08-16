a6901b40f419863a269dab9ab5106c963c7eb0d7
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-08-16 20:15:44 +0300

    Update Read.py
    Fix Read.py so that It now doesn't throw an error when reading an empty file

--- src/Read.py
+++ src/Read.py
@@ -1,8 +1,31 @@
 import json
+import os
 
 
 def json_read(name: str) -> dict:
+    """
+    Reads a json file with the given name and returns the data.
+    If the file does not exist, it will be created.
+    """
+    if not name.endswith(".json"):
+        name += ".json"
+    if not os.path.exists(name):
+        write_json(name, {})
+        return {}
     with open(name) as json_file:
         print(f"Reading {name}")
         data = json.load(json_file)
+        json_file.close()
     return data
+
+
+def write_json(name: str, data: dict[str, str]) -> None:
+    """
+    Writes a json file with the given name and data
+    """
+    if not name.endswith(".json"):
+        name += ".json"
+    with open(name, 'w+') as outfile:
+        json.dump(data, outfile, indent=4)
+        outfile.close()
+    print(f"Writing {name}")
