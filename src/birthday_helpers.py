82a2850fa120ff835d4627ef2f6a0bcf4e32e189
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-09-09 11:04:47 +0300

    Add logging to birthday helpers

--- src/birthday_helpers.py
+++ src/birthday_helpers.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+import logging
 
 
-def get_user_and_date_from_string(dictinary: dict):
+
+def get_user_and_date_from_string(dictinary: dict[int, str]):
   new_dict = {}
-  import src.client as client
+  import src.client as client  # we import here to avoid circular imports
   client = client.get_client_instance()
   for user_id, date in dictinary.items():
     user = client.get_user(int(user_id))
@@ -12,7 +14,7 @@ def get_user_and_date_from_string(dictinary: dict):
     dates = date.split("-")
     if len(dates) != 3:
       e = ValueError("Unknown date format, Please fix!")
-      print(e)
+      logging.error(e, date, dates, stack_info=True)
       continue
     date_obj = datetime(int(dates[0]), int(dates[1]), int(dates[2]))
     print(f"{user} : {date_obj}")
