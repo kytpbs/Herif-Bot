d3348e60c56a0ae8d4582090277d855351cd0be1
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-11-05 20:23:13 +0300

    add timeout to GPT request
    this should be better as now, when the request fails for some unknown reason, it won't halt the entire code... happened for many times for some reason

--- src/GPT.py
+++ src/GPT.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import threading
+from queue import LifoQueue
 import discord
 
 import openai
@@ -21,6 +23,8 @@ async def create_message_history(channel: discord.abc.Messageable, limit:int = 1
         message_history.append((message.author, message.content))
     return message_history
 
+def modified_create(queue: LifoQueue, *args, **kwargs):
+    queue.put(openai.ChatCompletion.create(*args, **kwargs))
 
 # noinspection PyBroadException
 def question(message: str, user_name: str = "MISSING", server_name: str = SERVER_NAME):
@@ -77,11 +81,19 @@ def chat(main_message: str, message_history: list[tuple[discord.User, str]]):
         "content": main_message,
         })
     try:
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=400,
-        )
+        queue = LifoQueue()
+        thread = threading.Thread(target=modified_create,
+                                        name="chat Request",
+                                        args=[queue],
+                                        kwargs={'model': "gpt-3.5-turbo",
+                                            'messages': messages,
+                                            'max_tokens': 400})
+        thread.start()
+        thread.join(30)
+        if thread.is_alive():
+            logging.error("thread timed out")
+            return ERROR2
+        response = queue.get()
     except openai.OpenAIError as error:
         logging.error(error, stack_info=True)
         return ERROR2
