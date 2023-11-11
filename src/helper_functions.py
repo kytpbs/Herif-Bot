cab26f27390a2a02264f45f36ee726ac19465ac6
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-11-11 17:53:47 +0300

    change spacing to 4 in helper_functions

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -2,7 +2,8 @@ import discord
 
 
 def get_general_channel(guild: discord.Guild):
-  for channel in guild.text_channels:
-    if "genel" in channel.name.lower() or "general" in channel.name.lower():
-      return channel
-  return None
+    for channel in guild.text_channels:
+        if "genel" in channel.name.lower() or "general" in channel.name.lower():
+            return channel
+    return None
+
