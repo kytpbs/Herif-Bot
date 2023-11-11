76bc2898d61cd24cef68cd2144a51eae7febe862
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-11-11 20:41:32 +0300

    add a diskDict class
    this class will handle reading and writing to the json file instead of forcing me to call write everytime...

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,4 +1,7 @@
+from typing import Any
 import discord
+
+import Read
 
 
 def get_general_channel(guild: discord.Guild):
@@ -8,3 +11,26 @@ def get_general_channel(guild: discord.Guild):
             return channel
     return None
 
+class DiskDict(dict):
+    def __init__(self, filename, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file = filename
+        self.load()
+
+    def save(self):
+        Read.write_json(self.file, self)
+
+    def load(self):
+        self.update(Read.json_read(self.file))
+
+    def __delitem__(self, __key: Any) -> None:
+        super().__delitem__(__key)
+
+    def __setitem__(self, __key: Any, __value: Any) -> None:
+        super().__setitem__(__key, __value)
+        self.save()
+
+    def __getitem__(self, __key: Any, load=False) -> Any:
+        if load:
+            self.load()
+        return super().__getitem__(__key)
