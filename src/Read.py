302d5051b8f740eeb95a0bb1d59ba618d13d7384
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-07-17 00:31:37 +0300

    add the ability to change the json folder name

--- src/Read.py
+++ src/Read.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 
+from Constants import JSON_FOLDER
+FILE_EXTENSION = ".json"
 
 def json_read(name: str) -> dict:
     """
@@ -10,17 +12,16 @@ def json_read(name: str) -> dict:
     Adds it to 'jsons' folder if it is not there.
     If the file does not exist, it will be created.
     """
-    if not name.startswith("jsons/"):
-        name = "jsons/" + name
-    if not name.endswith(".json"):
-        name += ".json"
+    if not name.startswith(JSON_FOLDER):
+        name = JSON_FOLDER + name
+    if not name.endswith(FILE_EXTENSION):
+        name += FILE_EXTENSION
     if not os.path.exists(name):
         write_json(name, {})
         return {}
     with open(name, encoding="utf-8") as json_file:
         logging.debug(f"Reading {name}")
         data = json.load(json_file)
-        json_file.close()
     return data
 
 
@@ -30,10 +31,10 @@ def write_json(name: str, data: dict[str, str]) -> None:
     Adds .json to the end of the name if it is not there.
     Adds it to 'jsons' folder if it is not there.
     """
-    if not name.startswith("jsons/"):
-        name = "jsons/" + name
-    if not name.endswith(".json"):
-        name += ".json"
+    if not name.startswith(JSON_FOLDER):
+        name = JSON_FOLDER + name
+    if not name.endswith(FILE_EXTENSION):
+        name += FILE_EXTENSION
     with open(name, 'w+', encoding="utf-8") as f:
         json.dump(data, f, indent=4)
         f.close()
