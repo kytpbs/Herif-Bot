66d3835c8d4cc651c70a18e6fabdc71e72ed41bb
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-07-30 23:50:15 +0300

    Add a `Downloader` interphase and `VideoFile` data type.
    This interphase allows for easier implementations of other methods to be added.
    Currently planned are:
    - Twitter (as that already exists in main right now)
    - Instagram (Might require using an account for reliable downloading)
    - Youtube (Already have most ready, just needs an additional function)

---
+++ src/downloader.py
@@ -0,0 +1,57 @@
+import logging
+from abc import ABC, abstractmethod
+
+_NONE_STRING = "Doesn't exist"
+
+
+class VideoFile:
+    """
+    Video object that contains the title, and its file path.
+    """
+    def __init__(self, file_path: str, title: str | None = None) -> None:
+        self._title = title
+        self._file_path = file_path
+
+    def __str__(self) -> str:
+        return f"Title: {self._title}, File Path: {self._file_path}"
+    
+    def __repr__(self) -> str:
+        return f'Title: {self._title or _NONE_STRING}, File Path: {self._file_path or _NONE_STRING}'
+
+    def __hash__(self) -> int:
+        return hash((self._title, self._file_path))
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, VideoFile):
+            return False
+        return self._title == other._title and self._file_path == other._file_path
+
+    @property
+    def caption(self) -> str | None:
+        return self._title
+    
+    @property
+    def path(self) -> str:
+        return self._file_path
+
+
+class VideoDownloader(ABC):
+    """
+    INTERPHASE FOR DOWNLOADING CONTENT FROM A WEBSITE
+    """
+
+    @staticmethod
+    @abstractmethod
+    def download_video_from_link(url: str, path: str | None = None) -> list[VideoFile]:
+        """
+        Downloads Videos from a url
+        if path is None, the default path is downloads/{website_name}
+
+        if the download fails, it returns an empty list
+        """
+        logging.error(
+            "VideoDownloader download_url interface was directly called, this should not happen! url was: %s for path: %s",
+            url,
+            path,
+        )
+        return []
