317aea96b6b399c44e98d73fe1dc07f6de8b7b8f
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-07-31 17:07:40 +0300

    create downloading_system
    this will also support Instagram and Youtube soon

---
+++ src/downloading_system.py
@@ -0,0 +1,18 @@
+import re
+from typing import Type
+
+from src.downloader import VideoDownloader
+from src.twitter import TwitterDownloader
+
+_TWITTER_REGEX = re.compile(r"\b(?:https?:\/\/)?(?:www\.)?(?:twitter\.com\/|t\.co\/|x\.com\/)\S*")
+_INSTAGRAM_REGEX = re.compile(r"\b(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/|instagr\.am\/)\S*")
+_YOUTUBE_REGEX = re.compile(r"\b(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/|youtu\.be\/)\S*")
+
+
+def get_downloader(url: str) -> Type[VideoDownloader] | None:
+    """
+    Returns the correct downloader for the given url
+    """
+    if re.match(_TWITTER_REGEX, url):
+        return TwitterDownloader
+    return None
