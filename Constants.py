a756858ee7c5d86d9513a5f653b9a68cc09d99b9
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-08-11 22:30:22 +0300

    lower download size limit to discord limits...
    The limit seems to be 8MBs but I can indeed send 18MB files which has to mean that it is up to 25, at least while using the `user-bot` interactions?

--- Constants.py
+++ Constants.py
@@ -1,4 +1,4 @@
-#pylint: disable=invalid-name  # it is normal to have the constants in a file named "Constants.py"
+# pylint: disable=invalid-name  # it is normal to have the constants in a file named "Constants.py"
 BOT_NAME = "O K B A"
 SERVER_NAME = "AIKO # उच्च शिक्षा परीक्षा"
 
@@ -21,4 +21,6 @@ GREEN = 0x00FF00
 FUNNY_COLOR = 696969
 
 RESPONSES_FILE = "responses.json"
-MAX_VIDEO_DOWNLOAD_SIZE: int = 80 # in MB, do not use anything other than an integer
+# default is 25 MB because that is the max file size for discord for now.
+# If you want to change this, make sure to check the current max upload limit for discord.
+MAX_VIDEO_DOWNLOAD_SIZE: int = 25  # in MB, do not use anything other than an integer
