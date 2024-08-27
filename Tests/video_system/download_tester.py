876e960177218d99a02d3c6b5159a63d77f35144
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-08-27 22:09:08 +0300

    refactor tests to use a single test function
    this will be modified in a second

--- Tests/video_system/download_tester.py
+++ Tests/video_system/download_tester.py
@@ -1,4 +1,6 @@
-from src.downloader import VIDEO_RETURN_TYPE
+import os
+import requests
+from src.downloader import VIDEO_RETURN_TYPE, VideoFile
 
 
 class DownloadTester:
@@ -6,16 +8,18 @@ class DownloadTester:
         assert len(videos) == 1
         video = videos[0]
 
-        with open(video.path, "rb") as downloaded, open(
-            should_be_path, "rb"
-        ) as should_be:
-            assert downloaded.read() == should_be.read(), "Downloaded file does not match the expected file"
+        self._test_download(video, should_be_path)
 
     def download_multiple_video_test(self, videos: VIDEO_RETURN_TYPE, should_be_paths: list[str]):
         assert len(videos) == len(should_be_paths), f"len(videos)={len(videos)} len(should_be_paths)={len(should_be_paths)}"
 
         for video, should_be_path in zip(videos, should_be_paths):
-            with open(video.path, "rb") as downloaded, open(
-                should_be_path, "rb"
+            self._test_download(video, should_be_path)
+
+
+    def _test_download(self, video: VideoFile, should_be_path: str):
+        with open(video.path, "rb") as downloaded, open(
+                should_be_path, "rb" # change to "wb" to run fix tests
             ) as should_be:
-                assert downloaded.read() == should_be.read(), "Downloaded file does not match the expected file"
+            # should_be.write(downloaded.read()) # uncomment to fix tests
+            assert downloaded.read() == should_be.read(), "Downloaded file does not match the expected file"
