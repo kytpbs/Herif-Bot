bd7bad0edc22eb430891b8ca19af2672044650eb
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-08-28 13:36:43 +0300

    add a downloader for unkown links
    this is thanks to yt_dlp supporting close to 250 or so links by default, so we can just use its abstraction to download our videos yipieee

---
+++ src/other.py
@@ -0,0 +1,24 @@
+import os
+from src.downloader import VIDEO_RETURN_TYPE, AlternateVideoDownloader
+
+
+class UnknownAlternateDownloader(AlternateVideoDownloader):
+    @classmethod
+    async def download_video_from_link(
+        cls, url: str, path: str | None = None
+    ) -> VIDEO_RETURN_TYPE:
+        if path is None:
+            path = os.path.join("downloads", "other")
+
+        os.makedirs(path, exist_ok=True)
+
+        ydt_opts = {
+            "format": "best",
+            "outtmpl": os.path.join(path, "%(id)s.%(ext)s"),
+            "noplaylist": True,
+            "default_search": "auto",
+            "nooverwrites": False, # We may have a video with the same id from a different source
+            "quiet": True,
+        }
+
+        return await cls._get_list_from_ydt(url, ydt_opts, path)
