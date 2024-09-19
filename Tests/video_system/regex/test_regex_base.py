5c00c66e7386676c373654d57249d6945642ba8c
Kaya <95276965+kytpbs@users.noreply.github.com>
2024-09-19 04:39:44 +0300

    pylint ignore spesific lines

--- Tests/video_system/regex/test_regex_base.py
+++ Tests/video_system/regex/test_regex_base.py
@@ -6,7 +6,7 @@ from src.download_system.downloading_system import get_downloader
 
 class TestDownloaderRegex:
     def setup(self, downloader_type_to_check: Type[VideoDownloader]) -> None:
-        self.downloader_type = downloader_type_to_check
+        self.downloader_type = downloader_type_to_check # pylint: disable=attribute-defined-outside-init
 
     def check_link(self, link: str):
         downloader = get_downloader(link)
