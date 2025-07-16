578d8b722bd254adf9504d51a81206470e009f65
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-07-16 23:43:32 +0200

    Use the new classes for implementation

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # import token_system first so that logging is setup correctly (ie. cloud logging) before everything else runs
 from src.token_system import get_token
 from src.client import get_client_instance
-from command_controller import setup_commands
 
 token = get_token()
 client = get_client_instance()
 
-setup_commands()
 
 client.run(token)
