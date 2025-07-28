8a4315cbc8052fd47dd9c4187c2f1d6c82ed7158
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-07-29 00:05:41 +0300

    Create the SQL implementation

--- src/data/providers/birthday_sql.py
+++ src/data/providers/birthday_sql.py
@@ -1,7 +1,8 @@
 import os
 from collections.abc import Mapping
 from datetime import date
-from typing import LiteralString
+
+from psycopg import sql
 
 from src.data.birtdays import (
     BirthdayAlreadyExists,
@@ -9,16 +10,15 @@ from src.data.birtdays import (
     Birthdays,
     BirthdayUnknownError,
     DBNotConnected,
+    GuildID,
+    UserID,
 )
-
-from psycopg import sql
-
 from src.sql.database import LOGGER, DatabaseClient
 
 
 class BirthdaySQL(Birthdays):
     @classmethod
-    async def create(cls, client: DatabaseClient) -> 'BirthdaySQL':
+    async def create(cls, client: DatabaseClient) -> "BirthdaySQL":
         instance = cls(client)
         await instance.start_up()
         return instance
@@ -55,53 +55,59 @@ class BirthdaySQL(Birthdays):
             UNIQUE(user_id, guild_id)
         )
 
-        """).format(
-            sql.Identifier(self._table_name)
-        )
+        """).format(sql.Identifier(self._table_name))
         # Use db_client instead of _client to avoid getting an error
         _ = await self._db_client.post(query)
         self._table_exists = True
         LOGGER.info(f"Table {self._table_name} created or already exists")
 
-    async def add_birthday(self, user_id: int, guild_id: int, birthday: str) -> None:
+    async def add_birthday(self, user_id: UserID, guild_id: GuildID, birthday: date) -> None:
         query = sql.SQL("""
             INSERT INTO {0} (user_id, guild_id, birthday)
             VALUES (%s, %s, %s)
             ON CONFLICT (user_id, guild_id) DO NOTHING
-            """
-        ).format(sql.Identifier(self._table_name))
+            """).format(sql.Identifier(self._table_name))
         # Checking rows affected lets us not check for the birthday and save a query
-        rows_affected = await self._client.post(
-            query, (user_id, guild_id, birthday)
-        )
+        rows_affected = await self._client.post(query, (user_id, guild_id, birthday))
         if rows_affected == 0:
             raise BirthdayAlreadyExists(f"Birthday for user {user_id} already exists")
         LOGGER.info(f"Added birthday for user {user_id} in guild {guild_id}")
 
-    async def remove_birthday(self, user_id: int, guild_id: int) -> None:
-        query = """
-            DELETE FROM %s WHERE user_id = %s AND guild_id = %s
-        """
-        rows_affected = await self._client.post(query, (self._table_name, user_id, guild_id))
+    async def remove_birthday(self, user_id: UserID, guild_id: GuildID) -> None:
+        query = sql.SQL("""
+            DELETE FROM {0} WHERE user_id = %s AND guild_id = %s
+        """).format(sql.Identifier(self._table_name))
+
+        rows_affected = await self._client.post(query, (user_id, guild_id))
         if rows_affected < 1:
             raise BirthdayDoesNotExist()
 
-    async def get_birthday(self, user_id: int, guild_id: int) -> date | None:
-        query = """
-            SELECT birthday FROM %s
+    async def get_birthday(self, user_id: UserID, guild_id: GuildID) -> date | None:
+        query = (
+            sql.SQL("""
+            SELECT birthday FROM {0}
             WHERE user_id = %s AND guild_id = %s
-            """
-        result = await self._client.get(query, (self._table_name, user_id, guild_id))
+        """)
+            .format(sql.Identifier(self._table_name))
+            .as_string(self._client.connection)
+        )
+
+        result = await self._client.get(query, (user_id, guild_id))
         if not result or not result[0] or not isinstance(result[0][0], date):
             return None
         return result[0][0]
 
-    async def get_all_birthdays(self, guild_id: int) -> dict[int, date]:
-        query = """
-            SELECT user_id, birthday FROM %s
+    async def get_all_birthdays(self, guild_id: GuildID) -> Mapping[int, date]:
+        query = (
+            sql.SQL("""
+            SELECT user_id, birthday FROM {0}
             WHERE guild_id = %s
-        """
-        result = await self._client.get(query, (self._table_name, guild_id))
+        """)
+            .format(sql.Identifier(self._table_name))
+            .as_string(self._client.connection)
+        )
+
+        result = await self._client.get(query, (guild_id,))
         if not result:
             return {}
         try:
@@ -110,15 +116,20 @@ class BirthdaySQL(Birthdays):
             raise BirthdayUnknownError("Somehow at least 2 rows did not exist") from e
 
     async def get_birthdays_on_date(
-        self, guild_id: int, date: date
-    ) -> Mapping[int, date]:
-        query = """
-            SELECT user_id, birthday FROM %s
+        self, guild_id: GuildID, date_: date
+    ) -> Mapping[UserID, date]:
+        query = (
+            sql.SQL("""
+            SELECT user_id, birthday FROM {0}
             WHERE guild_id = %s
             AND EXTRACT(DAY FROM birthday)=EXTRACT(DAY FROM %s)
             AND EXTRACT(MONTH FROM birthday)=EXTRACT(MONTH FROM %s)
-            """
-        result = await self._client.get(query, (self._table_name, guild_id, date, date))
+        """)
+            .format(sql.Identifier(self._table_name))
+            .as_string(self._client.connection)
+        )
+
+        result = await self._client.get(query, (guild_id, date_, date_))
         if not result:
             return {}
         try:
@@ -126,19 +137,22 @@ class BirthdaySQL(Birthdays):
         except (ValueError, TypeError, IndexError) as e:
             raise BirthdayUnknownError("Somehow at least 2 rows did not exist") from e
 
-    async def get_birthdays_today(self, guild_id: int) -> Mapping[int, date]:
-        query = """
-            SELECT user_id, birthday FROM %s
+    async def get_birthdays_today(self, guild_id: GuildID) -> Mapping[int, date]:
+        query = (
+            sql.SQL("""
+            SELECT user_id, birthday FROM {0}
             WHERE guild_id = %s
             AND EXTRACT(DAY FROM birthday)=EXTRACT(DAY FROM CURRENT_DATE)
             AND EXTRACT(MONTH FROM birthday)=EXTRACT(MONTH FROM CURRENT_DATE)
-            """
+        """)
+            .format(sql.Identifier(self._table_name))
+            .as_string(self._client.connection)
+        )
 
-        result = await self._client.get(query, (self._table_name, guild_id))
+        result = await self._client.get(query, (guild_id,))
         if not result:
             return {}
         try:
             return {row[0]: row[1] for row in result}
         except (ValueError, TypeError, IndexError) as e:
             raise BirthdayUnknownError("Somehow at least 2 rows did not exist") from e
-
