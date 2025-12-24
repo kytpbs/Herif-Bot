d5057b917211caa0f7d928a0b8093143cee0f6fa
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-12-25 00:13:54 +0100

    Put DB under an interface
    Should changing DBs in the future at least a bit easier, Maybe I should
    have just used SQLAlchemy but hey this time we gonna go Least Abstract
    -> Most Abstract, except I have the library handleing the pooling
    system, removing the entire premise of learning but eh...

--- src/data/data_manager.py
+++ src/data/data_manager.py
@@ -1,7 +1,15 @@
+import logging
+from typing import Final, Protocol
+
+import discord
+
+from src.sql.postgres import PostgresDBClient
 from src.data.birtdays import BirthdayProvider
 from src.data.providers.birthday_factory import BirthdayFactory
 from src.sql.database import DatabaseClient
+from src.sql.errors import NotConnectedError
 
+_LOGGER = logging.getLogger("DataManager")
 
 
 class DataManager:
@@ -9,9 +17,26 @@ class DataManager:
     Central place for all dynamic data that the codebase uses.
     """
     def __init__(self):
-        self._db_client = DatabaseClient()
-        self._birthday_factory: BirthdayFactory = BirthdayFactory(self._db_client)
+        self._db_client: DatabaseClient | None = None
+        try:
+            self._db_client = PostgresDBClient()
+        except NotConnectedError:
+            _LOGGER.warning(
+                "Database not connected, will pass None to all data providers"
+            )
+        self._birthday_factory: Final = BirthdayFactory(self._db_client)
 
     @property
     async def birthday_provider(self) -> BirthdayProvider:
         return await self._birthday_factory.create_birthday_provider
+
+
+class DiscordClientWithDataManager(discord.Client):
+    @property
+    def data_manager(self) -> DataManager:
+        ...
+
+class DataManagerProvider(Protocol):
+    @property
+    def data_manager(self) -> DataManager:
+        ...
---
+++ src/sql/postgres.py
@@ -0,0 +1,195 @@
+import logging
+import os
+from collections.abc import AsyncGenerator, Mapping, Sequence
+from contextlib import asynccontextmanager
+from typing import Any, Final, LiteralString, cast
+from frozendict import frozendict
+from typing_extensions import override
+
+import psycopg
+import psycopg_pool
+from async_lru import alru_cache
+from psycopg import sql
+from psycopg.rows import TupleRow
+from dotenv import load_dotenv
+
+from src.sql.database import DatabaseClient, Params, Query
+from src.sql.errors import NotConnectedError, SQLFailedMiserably
+
+_LOGGER = logging.getLogger("SQL")
+_ = load_dotenv()
+
+
+class PostgresDBClient(DatabaseClient):
+    def __init__(self, conn_str: str | None = None):
+        """
+        Initializes the database client.
+
+        Will lazy-load the connection pool on first use.
+        If you want to eagerly load, call :meth:`setup()` after initialization.
+
+        Args:
+            conn_str (str | None): The connection string to use.
+                If None, will attempt to read from the environment variable
+        """
+        conn_str = (
+            conn_str
+            or os.getenv("SQL_CONNECTION_STRING")
+            or self._create_conn_str_from_env()
+        )
+
+        self._pool: Final = psycopg_pool.AsyncConnectionPool(
+            conn_str, min_size=0, max_size=10, open=False
+        )
+
+        self._has_setup: bool = False
+
+    @override
+    async def setup(self):
+        """
+        Sets up the database client, opening the connection pool.
+        Normally not needed, as the pool will lazy-load on first use.
+        """
+        await self._pool.open()
+        self._has_setup = True
+
+    @override
+    async def close(self):
+        """
+        Closes the connection to the database.
+        """
+        await self._pool.close()
+
+    @property
+    @asynccontextmanager
+    @override
+    async def connection(self) -> AsyncGenerator[psycopg.AsyncConnection, None]:
+        await self._pool.open()
+        async with self._pool.connection() as conn:
+            yield conn
+
+    @property
+    @asynccontextmanager
+    @override
+    async def cursor(
+        self,
+    ) -> AsyncGenerator[tuple[psycopg.AsyncConnection, psycopg.AsyncCursor], None]:
+        async with self.connection as conn:
+            async with conn.cursor() as cursor:
+                yield (conn, cursor)
+
+    def _create_conn_str_from_env(self) -> str:
+        user = os.getenv("SQL_USER", "postgres")
+        password = os.getenv("SQL_PASSWORD")
+        database = os.getenv("SQL_DATABASE", "herifbot")
+        host = os.getenv("SQL_HOST", "localhost")
+        port = os.getenv("SQL_PORT", "5432")
+        sslmode = os.getenv("SQL_SSLMODE", "require")
+        channel_binding = os.getenv("SQL_CHANNELBINDING", "require")
+
+        if not password:
+            _LOGGER.error("No password found in environment variables")
+            raise NotConnectedError("No password found in environment variables")
+
+        return f"postgresql://{user}:{password}@{host}:{port}/{database}?sslmode={sslmode}&channel_binding={channel_binding}"
+
+    @override
+    async def post(
+        self, query: Query | sql.SQL, params: Params[Any] | None = None
+    ) -> int:
+        """
+        Executes a query and returns the number of rows affected by the query.
+        This should be used for INSERT, UPDATE, and DELETE queries.
+
+        If you want to get the result of a query, use :meth:`get()`.
+
+        Args:
+            query (str): The query to execute.
+            params (tuple | list | dict | None): The parameters to pass to the query.
+                Defaults to None.
+
+        Returns:
+            int: The number of rows affected by the query.
+        """
+        async with self.cursor as (_, cursor):
+            _ = await cursor.execute(query, params)
+
+            return cursor.rowcount
+
+    @override
+    async def get(
+        self, query: Query, params: Params[Any] | None = None
+    ) -> list[TupleRow] | None:
+        match params:
+            case Mapping():
+                new_params = cast(frozendict[str,Any], frozendict(params))
+            case Sequence():
+                new_params = tuple(params)
+            case None:
+                new_params = None
+        match query:
+            case sql.SQL() | sql.Composed():
+                query_str = query.as_string()
+            case LiteralString():
+                query_str = query
+        return await self._get_cached(query_str, new_params)
+
+    @alru_cache(maxsize=32, typed=True, ttl=0.1)
+    async def _get_cached(
+        self, query: Query, params: Params[Any] | None = None
+    ) -> list[TupleRow] | None:
+        return await self._get(query, params)
+
+    # Cache the `get` method to avoid unnecessary database queries.
+    # But do with TTL (Time To Live) to avoid caching forever.
+    # Cache for 100 milliseconds
+    # Should be more than enough for sudden bursts from func calls
+    # The rest we don't need to cache for now anyways
+    async def _get(
+        self, query: Query, params: Params[Any] | None = None
+    ) -> list[TupleRow] | None:
+        """
+        Executes a query and returns the first result.
+
+        This doesn't accept mapping types, due to caching issues.
+
+        Update and create a FrozenDict type to fix this.
+        Args:
+            query (Query): The query to execute.
+            params (Sequence | None): The parameters to pass to the query.
+                Defaults to None.
+
+        Returns:
+            psycopg.AsyncResult: The result of the query.
+        """
+        _LOGGER.debug("Executing query: '%s' with values: %s", query, params)
+
+        async with self.cursor as (_, cursor):
+            try:
+                _ = await cursor.execute(query, params)
+                _LOGGER.debug("Query ran successfully")
+                return await cursor.fetchall()
+            except psycopg.ProgrammingError:
+                _LOGGER.debug("Query returned no results")
+                return None
+            except psycopg.Error as e:
+                _LOGGER.error(
+                    "Failed to run query: '%s', with values '%s'. Error: %s",
+                    query,
+                    params,
+                    e,
+                )
+                raise SQLFailedMiserably("Query failed") from e
+
+
+async def _main():
+    client = PostgresDBClient()
+    print(await client.get("SELECT version()"))
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(_main())
