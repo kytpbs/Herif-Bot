a5a31abc2004717b7a30014454a042334bf5aaac
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-12-25 02:10:01 +0100

    Implement SQL for Customizations

--- src/commands/customization.py
+++ src/commands/customization.py
@@ -1,12 +1,16 @@
+import logging
+from itertools import islice
+
 import discord
 from discord import app_commands
 
 from Constants import CYAN
-from src import client
+from src.data.data_manager import DiscordClientWithDataManager
 from src.commands.command_group import CommandGroup, CommandList
+from src.data.customizations import CustomizationError
 
-# Hopefully this will be moved behind an interface without an implementation detail in the command group
-custom_responses = client.get_custom_responses()
+_LOGGER = logging.getLogger("Commands.Customization")
+
 
 @app_commands.allowed_installs(guilds=True, users=False)
 @app_commands.allowed_contexts(guilds=True, dms=False, private_channels=False)
@@ -14,59 +18,148 @@ class CustomizationCommands(app_commands.Group, CommandGroup):
     @classmethod
     def get_commands(cls) -> CommandList:
         return [
-                cls(name="özel", description="Bota özel komutlar ekleyip görmen için komutlar"),
+            cls(
+                name="özel",
+                description="Bota özel komutlar ekleyip görmen için komutlar",
+            ),
         ]
 
-    @app_commands.command(name="olustur", description="botun senin ayarladığın mesajlara cevap verebilmesini sağlar")
-    async def create_command(self, interaction: discord.Interaction, text: str, answer: str, degistir: bool = False):
-        if custom_responses.get(text) is None:
-            custom_responses[text] = answer
-            await interaction.response.send_message(f"Yeni bir cevap oluşturuldu. {text} : {answer}")
+    @app_commands.command(
+        name="olustur",
+        description="botun senin ayarladığın mesajlara cevap verebilmesini sağlar",
+    )
+    async def create_command(
+        self,
+        interaction: discord.Interaction[DiscordClientWithDataManager],
+        text: str,
+        answer: str,
+    ):
+        if not interaction.guild_id:
+            _ = await interaction.response.send_message(
+                "Bu komut sadece sunucularda kullanılabilir", ephemeral=True
+            )
             return
 
-        if not isinstance(interaction.user, discord.Member) or not interaction.user.guild_permissions.administrator:
-            await interaction.response.send_message(f"Bu mesaja zaten bir cevap var: {custom_responses[text]}, " +
-                                                    "lütfen başka bir mesaj deneyin",
-                                                    ephemeral=True)
+        customs_provider = await interaction.client.data_manager.customization_provider
+        response = await customs_provider.get_response(interaction.guild_id, text)
+
+        if response:
+            _ = await interaction.response.send_message(
+                f"Bu mesaja zaten bir cevap var: {response}, "
+                + "lütfen başka bir mesaj deneyin"
+                if response.added_by_user_id != interaction.user.id
+                else "değiştirmek için önce olan cevabı silmelisiniz",
+                ephemeral=True,
+            )
             return
 
-        if not degistir:
-            await interaction.response.send_message(f"Bu mesaja zaten bir cevap var: {custom_responses[text]}, " +
-                                                    "değiştirmek için komutta 'degistir' argümanını kullanın",
-                                                    ephemeral=True)
+        await customs_provider.create_custom_command(
+            interaction.guild_id, text, answer, interaction.user.id
+        )
+        embed = discord.Embed(
+            title="Cevap Oluşturuldu",
+            description=f"'_{text}_': '_{answer}_' adlı cevap oluşturuldu",
+            color=CYAN,
+        )
+
+        _ = await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(
+        name="cevaplar", description="Bütün özel eklenmiş cevapları gösterir"
+    )
+    async def answers(self, interaction: discord.Interaction[DiscordClientWithDataManager]):
+        if not interaction.guild_id:
+            _ = await interaction.response.send_message(
+                "Bu komut sadece sunucularda kullanılabilir", ephemeral=True
+            )
+            return
+        customs_provider = await interaction.client.data_manager.customization_provider
+        responses = await customs_provider.get_all_custom_commands(
+            interaction.guild_id, limit=26
+        )  # limit to 26 to check if there are more than 25
+        if not responses:
+            _ = await interaction.response.send_message(
+                "Bu sunucuda özel eklenmiş cevap yok", ephemeral=True
+            )
             return
 
-
-        eski_cevap = custom_responses[text]
-        custom_responses[text] = answer
-        embed = discord.Embed(title="Cevap Değiştirildi", description=f"'{text} : {answer}' a değiştirildi", color=CYAN)
-        embed.add_field(name="Eski Cevap", value=eski_cevap, inline=False)
-        await interaction.response.send_message(embed=embed)
-
-    @app_commands.command(name="cevaplar", description="Bütün özel eklenmiş cevapları gösterir")
-    async def answers(self, interaction: discord.Interaction):
         embed = discord.Embed(title="Özel Cevaplar", color=CYAN)
-        embed.description = "Özel eklenmiş cevaplar"
-        for i, (key, value) in enumerate(custom_responses.items()):
-            if i >= 24:
-                embed.description += "\n\nDaha fazla cevap var, 25 ten fazlası gösterilmiyor"
-                break
-            embed.add_field(name=key, value=value, inline=False)
-        await interaction.response.send_message(embed=embed)
+        embed.description = (
+            "Özel eklenmiş cevaplar"
+            if len(responses) <= 24
+            else "Özel eklenmiş cevapların bir kısmı (gösterilen 24 cevap) \n\n"
+        )
 
+        for trigger, response in islice(responses.items(), 24):
+            _ = embed.add_field(name=trigger, value=response, inline=False)
+        _ = await interaction.response.send_message(embed=embed)
 
+    async def _delete_autocomplete(
+        self,
+        interaction: discord.Interaction[DiscordClientWithDataManager],
+        current: str,
+    ) -> list[app_commands.Choice[str]]:
+        if not interaction.guild_id or not isinstance(interaction.user, discord.Member):
+            return []
+        customs_provider = await interaction.client.data_manager.customization_provider
+        responses = await customs_provider.get_all_custom_commands(interaction.guild_id)
+        choices = [
+            app_commands.Choice(
+                name=f"{cmd}: {response}"[:97] + ("..." if len(cmd + str(response)) > 97 else ""),
+                value=cmd,
+            )
+            for cmd, response in responses.items()
+            if (
+                current.lower() in cmd.lower()
+                and (
+                    interaction.user.guild_permissions.administrator
+                    or response.added_by_user_id == interaction.user.id
+                )
+            )
+        ]
+        return choices
+
+    @app_commands.autocomplete(trigger=_delete_autocomplete)
     @app_commands.command(name="sil", description="Özel eklenmiş bir cevabı siler")
-    @app_commands.checks.has_permissions(administrator=True)
-    async def delete_command(self, interaction: discord.Interaction, text: str):
-        if custom_responses.get(text) is None:
-            await interaction.response.send_message(f"Cevap bulunamadı: {text}", ephemeral=True)
+    async def delete_command(
+        self, interaction: discord.Interaction[DiscordClientWithDataManager], trigger: str
+    ):
+        if not interaction.guild_id or not isinstance(interaction.user, discord.Member):
+            _ = await interaction.response.send_message(
+                "Bu komut sadece sunucularda kullanılabilir", ephemeral=True
+            )
+            return
+        customs_provider = await interaction.client.data_manager.customization_provider
 
-        user: discord.Member = interaction.user # type: ignore
-        if user.guild_permissions.administrator is False:
-            await interaction.response.send_message(f"{text} mesajına zaten cevap var ve sen bu komutu kullanamazsın")
+        response = await customs_provider.get_response(interaction.guild_id, trigger)
+
+        if response is None:
+            _ = await interaction.response.send_message(
+                f"'_{trigger}_' için cevap bulunamadı", ephemeral=True
+            )
             return
 
-        response = custom_responses[text]
-        del custom_responses[text]
-        embed = discord.Embed(title="Cevap Silindi", description=f"'{text}: {response}' adlı cevap silindi", color=CYAN)
-        await interaction.response.send_message(embed=embed)
+        if (
+            not interaction.user.guild_permissions.administrator
+            and response.added_by_user_id != interaction.user.id
+        ):
+            _ = await interaction.response.send_message(
+                f"{trigger} mesajına olan cevabı silme yetkiniz yok", ephemeral=True
+            )
+            return
+
+        try:
+            await customs_provider.delete_custom_command(interaction.guild_id, trigger)
+        except CustomizationError as e:
+            _LOGGER.error(f"Özel cevap silinirken hata oluştu: {e}")
+            _ = await interaction.response.send_message(
+                f"'{trigger}' adlı cevabı silerken bir hata oluştu", ephemeral=True
+            )
+            return
+
+        embed = discord.Embed(
+            title="Cevap Silindi",
+            description=f"'_{trigger}_: _{response}_' adlı cevap silindi",
+            color=CYAN,
+        )
+        _ = await interaction.response.send_message(embed=embed)
