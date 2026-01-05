e547eeaed8f45c09bb5b6818fe8f82f15f99f7b3
Kaya <95276965+kytpbs@users.noreply.github.com>
2026-01-05 03:44:35 +0100

    add birthdayConfig data to birthday provider
    This will be usefull when doing the Tasks

--- src/data/birthdays.py
+++ src/data/birthdays.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from collections.abc import Mapping
+from dataclasses import dataclass
 from datetime import UTC, date, datetime
 from typing import TypeAlias
 
@@ -30,9 +31,18 @@ class BirthdayDoesNotExist(BirthdayError):
 class BirthdayUnknownError(SQLFailedMiserably, BirthdayError):
     pass
 
+
 UserID: TypeAlias = int
 GuildID: TypeAlias = int
 Birthday: TypeAlias = date
+
+
+@dataclass
+class BirthdayConfig:
+    """Represents the configuration for a guild's birthdays"""
+
+    channel_id: int
+    role_id: int | None = None
 
 
 class BirthdayProvider(ABC):
@@ -64,6 +74,7 @@ class BirthdayProvider(ABC):
 
     @abstractmethod
     async def get_birthdays(self, guild_id: GuildID) -> Mapping[UserID, Birthday]:
+        """Get all distinct birthdays for a guild"""
         pass
 
     @abstractmethod
@@ -72,6 +83,40 @@ class BirthdayProvider(ABC):
     ) -> Mapping[UserID, Birthday]:
         pass
 
+    @abstractmethod
+    async def get_all_birthdays_on_date(
+        self, date_: Birthday
+    ) -> Mapping[GuildID, Mapping[UserID, Birthday]]:
+        pass
+        # TODO: do this...
+
+    @abstractmethod
+    async def set_birthday_config(
+        self, guild_id: GuildID, config: BirthdayConfig
+    ) -> None:
+        pass
+
+    @abstractmethod
+    async def get_birthday_config(self, guild_id: GuildID) -> BirthdayConfig | None:
+        """Gets the guild's birthday configuration returning None if not setup
+
+        Args:
+            guild_id (GuildID): The ID of the guild whose birthday configuration is to be retrieved
+
+        Returns:
+            BirthdayConfig | None: The birthday configuration for the specified guild, or None if no configuration exists
+        """
+
+    @abstractmethod
+    async def remove_birthday_config(self, guild_id: GuildID) -> None:
+        """Removes the birthday configuration for a specific guild. Meaning disabling birthdays in that guild.
+
+        Args:
+            guild_id (GuildID(int)): The ID of the guild whose configuration is to be removed
+
+        Raises:
+            BirthdayDoesNotExist: If no configuration exists for the specified guild
+        """
+
     async def get_birthdays_today(self, guild_id: GuildID) -> Mapping[UserID, Birthday]:
-        date.today()
         return await self.get_birthdays_on_date(guild_id, datetime.now(UTC).date())
