21d6f3be271907c7b93e85a044a6c23d389600fa
Kaya <95276965+kytpbs@users.noreply.github.com>
2026-01-06 21:38:41 +0100

    do some refactoring for customizations

--- Tests/database_system/test_customization_sql.py
+++ Tests/database_system/test_customization_sql.py
@@ -4,9 +4,8 @@ from sys import platform
 import pytest
 
 from src.sql.postgres import PostgresDBClient
-from src.data.customizations import CustomizationDoesNotExist
+from src.data.customizations import CustomCommand, CustomizationAlreadyExists, CustomizationDoesNotExist
 from src.data.providers.customization_sql import CustomizationSQL
-from src.sql.database import DatabaseClient
 
 
 @pytest.fixture(scope="module")
@@ -28,17 +27,21 @@ async def test_custom_command_crud():
     response = "pytest response"
 
     try:
+        command = CustomCommand(guild_id, command_input, response, added_by_user_id=999)
         await customizations.create_custom_command(
-            guild_id, command_input, response, added_by_user_id=999
+            command.guild_id,
+            command.command_input,
+            command.response,
+            added_by_user_id=command.added_by_user_id,
         )
 
-        assert await customizations.get_response(guild_id, command_input) == response
+        assert await customizations.get_response(guild_id, command_input) == command
         assert await customizations.get_all_custom_commands(guild_id) == {
-            command_input: response
+            command_input: command
         }
 
         await customizations.delete_custom_command(guild_id, command_input)
-        await asyncio.sleep(0.1)  # Ensure cache invalidation
+        await asyncio.sleep(0.5)  # Ensure cache invalidation
 
         assert await customizations.get_response(guild_id, command_input) is None
         assert await customizations.get_all_custom_commands(guild_id) == {}
@@ -57,14 +60,16 @@ async def test_duplicate_custom_command_addition():
     guild_id = 0
     command_input = "pytest_duplicate_command"
     response = "first response"
+    command = CustomCommand(guild_id, command_input, response)
 
     try:
         await customizations.create_custom_command(guild_id, command_input, response)
-        # Second insert should be ignored due to ON CONFLICT DO NOTHING
-        await customizations.create_custom_command(guild_id, command_input, "new value")
+        # Second insert should raise exception
+        with pytest.raises(CustomizationAlreadyExists):
+            await customizations.create_custom_command(guild_id, command_input, "new value")
 
         commands = await customizations.get_all_custom_commands(guild_id)
-        assert commands == {command_input: response}
+        assert commands == {command_input: command}
     finally:
         try:
             await customizations.delete_custom_command(guild_id, command_input)
