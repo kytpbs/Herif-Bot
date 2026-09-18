ee0b98308f8da3d23ec5b1e2abfc50d9fc04cc4b
Kaya <kytpbs@gmail.com>
2026-09-18 20:52:54 +0200

    add type hinting to DiskDict

--- src/Helpers/helper_functions.py
+++ src/Helpers/helper_functions.py
@@ -1,13 +1,11 @@
 import atexit
 from datetime import UTC, datetime, timedelta
 import logging
-from typing import Any
+from typing import Any, TypeVar
 import discord
 
 from src.Helpers.global_errors import NoGuildContextInGuildCommandError
 from src import Read
-
-
 
 
 def assert_guild_membered(
@@ -15,7 +13,8 @@ def assert_guild_membered(
 ) -> tuple[discord.Member, int]:
     if not isinstance(interaction.user, discord.Member) or not interaction.guild_id:
         raise NoGuildContextInGuildCommandError(
-            "This command can only be used in a guild context")
+            "This command can only be used in a guild context"
+        )
         # Raise a new error type and catch in client
     return (interaction.user, interaction.guild_id)
 
@@ -27,20 +26,31 @@ def get_general_channel(guild: discord.Guild):
             return channel
     return None
 
-async def get_deleting_person(message: discord.Message) -> discord.Member | discord.User:
+
+async def get_deleting_person(
+    message: discord.Message,
+) -> discord.Member | discord.User:
     if message.guild is None:
         return message.author
 
-    async for entry in message.guild.audit_logs(action=discord.AuditLogAction.message_delete, after=datetime.now(UTC) - timedelta(minutes=2)):
-        logging.debug(f'{entry.user} deleted {entry.target} at {entry.created_at}')
+    async for entry in message.guild.audit_logs(
+        action=discord.AuditLogAction.message_delete,
+        after=datetime.now(UTC) - timedelta(minutes=2),
+    ):
+        logging.debug(f"{entry.user} deleted {entry.target} at {entry.created_at}")
         if entry.user is not None:
             return entry.user
 
     # if we can't find who deleted the message, it was probably the author
     return message.author
 
-class DiskDict(dict):
-    def __init__(self, filename, *args, **kwargs):
+
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+class DiskDict(dict[K, V]):
+    def __init__(self, filename: str, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.filename = filename
         self.load()
@@ -52,21 +62,22 @@ class DiskDict(dict):
     def load(self):
         self.update(Read.json_read(self.filename))
 
-    def __delitem__(self, __key: Any) -> None:
+    def __delitem__(self, __key: K) -> None:
         super().__delitem__(__key)
 
-    def __setitem__(self, __key: Any, __value: Any) -> None:
+    def __setitem__(self, __key: K, __value: V) -> None:
         super().__setitem__(__key, __value)
         self.save()
 
-    def __getitem__(self, __key: Any, load=False) -> Any:
+    def __getitem__(self, __key: K, load: bool = False) -> V:
         if load:
             self.load()
         return super().__getitem__(__key)
 
     def __enter__(self):
-        self.load() # load the file before returning self, as why would they use it using "with" if they didn't want to load it?
+        self.load()  # load the file before returning self, as why would they use it using "with" if they didn't want to load it?
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.save() # save the file before exiting the "with" block
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any):
+        del exc_type, exc_val, exc_tb
+        self.save()  # save the file before exiting the "with" block
